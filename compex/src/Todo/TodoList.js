2e3fadea1be8db2fc1b47655155062625d797495
merryinham <merryinham@gmail.com>
2019-08-07 16:00:54 +0900

    todocommit

---
+++ compex/src/Todo/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const TodoList=({todos})=>{
+
+    const list= todos.map(({tno,title}) => <li key={tno}>{title}</li>)
+
+    return(
+        <ul>
+            {list}
+        </ul>
+    )
+
+}
+export default TodoList
